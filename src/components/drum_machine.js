d181b1f51b2c8cdd79bf6d3ac791724d86600cd1
Kevin Reynolds <Kevin.T.Reynolds30@gmail.com>
2018-03-27 12:31:29 -0700

    add sounds/refactor drumpad array

--- src/components/drum_machine.js
+++ src/components/drum_machine.js
@@ -3,8 +3,18 @@ import DrumPad from './drum_pad'
 
 
 const DrumMachine = () => {
-    const drums = ['clap','hihat','kick','openhat','boom','ride','snare','tom','tink'];
-    const drumPads = drums.map(drum => <DrumPad drum={drum} />)
+    const drums = [
+        ['clap','Q'],
+        ['hihat','W'],
+        ['kick','E'],
+        ['openhat','A'],
+        ['boom','S'],
+        ['ride','D'],
+        ['snare','Z'],
+        ['tom','X'],
+        ['tink','C']
+    ];
+    const drumPads = drums.map(drum => <DrumPad drum={drum[0]} key={drum[1]} />)
     return (
         <div id="drum-machine">
             <div id="display" />
