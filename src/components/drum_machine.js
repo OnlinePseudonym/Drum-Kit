9bbb135b8c80e463e45b591effda465cee5d8a03
Kevin Reynolds <Kevin.T.Reynolds30@gmail.com>
2018-03-27 14:32:33 -0700

    play sound onclick

--- src/components/drum_machine.js
+++ src/components/drum_machine.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import DrumPad from './drum_pad'
+import clap from '../sounds/clap.wav'
+import hihat from '../sounds/hihat.wav'
+import kick from '../sounds/kick.wav'
+import openhat from '../sounds/openhat.wav'
+import boom from '../sounds/boom.wav'
+import ride from '../sounds/ride.wav'
+import snare from '../sounds/snare.wav'
+import tom from '../sounds/tom.wav'
+import tink from '../sounds/tink.wav'
 
 
-const DrumMachine = () => {
+const DrumMachine = (props) => {
     const drums = [
-        ['clap','Q'],
-        ['hihat','W'],
-        ['kick','E'],
-        ['openhat','A'],
-        ['boom','S'],
-        ['ride','D'],
-        ['snare','Z'],
-        ['tom','X'],
-        ['tink','C']
+        [clap,'Q'],
+        [hihat,'W'],
+        [kick,'E'],
+        [openhat,'A'],
+        [boom,'S'],
+        [ride,'D'],
+        [snare,'Z'],
+        [tom,'X'],
+        [tink,'C']
     ];
-    const drumPads = drums.map(drum => <DrumPad drum={drum[0]} key={drum[1]} />)
+    const drumPads = drums.map(drum => <DrumPad key={drum[1]} drum={drum[0]} keyPress={drum[1]} />)
     return (
         <div id="drum-machine">
             <div id="display" />
--- src/components/drum_pad.js
+++ src/components/drum_pad.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const DrumPad = (props) => {
-    return (
-        <div>{props.drum}</div>
-    )
+class DrumPad extends Component {
+    constructor(props) {
+        super(props)
+        
+        this.playSound = this.playSound.bind(this);
+    }
+    
+    playSound = (e) => {
+        const audio = document.getElementById(this.props.keyPress);
+        audio.currentTime = 0;
+        audio.play();
+    }
+
+    render() {
+        return (
+            <div id={this.props.drum} onClick={this.playSound}>
+                {this.props.keyPress}
+                <audio src={this.props.drum} id={this.props.keyPress} />
+            </div>
+        )
+    }
 }
 
 export default DrumPad;
